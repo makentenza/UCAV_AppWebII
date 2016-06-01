24334fe25bc1d8d56eac6ced7b55f19b18441f3f
Marcos Entenza <makentenza@gmail.com>
2016-06-01 22:51:16 +0100

    Many changes including spanish codig as language variable definition

--- apps/settings.py
+++ apps/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
